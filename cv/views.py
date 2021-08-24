f3dc5ed73f6a70dfc0b3c6eb2e644a9a2655811d
rainfidelis <rainnyfidelis@yahoo.com>
2021-08-24 16:16:17 +0100

    modified thank-you page

--- cv/views.py
+++ cv/views.py
@@ -1,8 +1,9 @@
 from django.http.response import BadHeaderError
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import ContactForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.mail import send_mail
+from django.contrib import messages
 from config.settings import DEFAULT_FROM_EMAIL
 
 
@@ -11,20 +12,21 @@ def home(request):
 
     submitted = False
     form = ContactForm(request.POST)
-    if request.method == "POST":
-        if form.is_valid():
-            subject = form.cleaned_data['subject']
-            body = {
-                'name': form.cleaned_data['name'],
-                'email': form.cleaned_data['email'],
-                'message': form.cleaned_data['message']
-            }
-            message = "\n".join(body.values())
-            try:
-                send_mail(subject, message, DEFAULT_FROM_EMAIL, ['rainnyfidelis@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return HttpResponseRedirect('/thank-you')
+    if request.method == "POST" and form.is_valid():
+        subject = form.cleaned_data['subject']
+        body = {
+            'name': form.cleaned_data['name'],
+            'email': form.cleaned_data['email'],
+            'message': form.cleaned_data['message']
+        }
+        message = "\n".join(body.values())
+        try:
+            send_mail(subject, message, DEFAULT_FROM_EMAIL, ['rainnyfidelis@gmail.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+
+        # messages.add_message(request=request, message='Thanks! Your message was well received. Will reply shortly.', level=messages.SUCCESS)
+        return redirect('/thank-you')
 
     else:
         form = ContactForm()

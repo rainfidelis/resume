febafed9d5f9b61a73fe4c8f70458c3baecf6bc7
rainfidelis <rainnyfidelis@yahoo.com>
2021-08-24 15:52:49 +0100

    email configuration for contact form

--- config/settings.py
+++ config/settings.py
@@ -130,8 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email backend config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'fidelisrain@gmail.com'
-EMAIL_HOST_PASSWORD = 'key'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'default from email'
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
--- cv/views.py
+++ cv/views.py
@@ -1,22 +1,33 @@
+from django.http.response import BadHeaderError
 from django.shortcuts import render
 from .forms import ContactForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.mail import send_mail
+from config.settings import DEFAULT_FROM_EMAIL
 
 
 
 def home(request):
 
     submitted = False
-
+    form = ContactForm(request.POST)
     if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid:
-            form.save()
-            submitted = True
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            body = {
+                'name': form.cleaned_data['name'],
+                'email': form.cleaned_data['email'],
+                'message': form.cleaned_data['message']
+            }
+            message = "\n".join(body.values())
+            try:
+                send_mail(subject, message, DEFAULT_FROM_EMAIL, ['rainnyfidelis@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
             return HttpResponseRedirect('/thank-you')
 
     else:
-        form = ContactForm
+        form = ContactForm()
 
     return render(request, 'index.html', {'form': form, 'submitted': submitted})
 
